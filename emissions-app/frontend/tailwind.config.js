6aa0760c331190a87a880925a283a6d0efa68541
Jared Porcenaluk <jporcenaluk@gmail.com>
2024-10-14 04:32:10 +0100

    added fonts

--- emissions-app/frontend/tailwind.config.js
+++ emissions-app/frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        header: ['Indie Flower', 'serif'],  // For h1, h2, etc.
+        body: ['Afacad Flux', 'sans-serif'],  // For the rest of the body text
+      },
+    },
   },
   plugins: [ require('daisyui') ],
 }
