45870c079bef1b2f0b21076b3e4d42b27f0e1579
Jared Porcenaluk <jporcenaluk@gmail.com>
2024-10-15 08:42:06 +0100

    added button to navigate to stats

--- emissions-app/frontend/src/components/Footer.tsx
+++ emissions-app/frontend/src/components/Footer.tsx
@@ -1,7 +1,20 @@
-const Footer = () => (
-    <footer className="bg-gray-900 text-white p-4">
-      Made by <a className="underline" href="https://jaredporcenaluk.com">Jared Porcenaluk</a> & <a className="underline" href="https://sarahporcenaluk.com">Sarah Porcenaluk</a>. Made with the assistance of Gen AI.
+import { useNavigate } from "react-router-dom";
+
+function Footer() {
+
+  const navigate = useNavigate()
+  const handleNavigateStats = () => {
+    navigate('/stats');
+  };
+
+  return (
+    <footer className="bg-gray-900 text-white p-4 flex justify-between items-center">
+      <div>
+        Made by <a className="underline" href="https://jaredporcenaluk.com">Jared Porcenaluk</a> & <a className="underline" href="https://sarahporcenaluk.com">Sarah Porcenaluk</a>. Made with the assistance of Gen AI.
+      </div>
+      <button className="underline" onClick={handleNavigateStats}>Stats</button>
     </footer>
-)
+  )
+}
 
 export default Footer;
