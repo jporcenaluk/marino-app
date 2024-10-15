b2e5e5814e038f77b9cd217da350953206320ffe
Jared Porcenaluk <jporcenaluk@gmail.com>
2024-10-15 08:36:55 +0100

    link to home in the header

--- emissions-app/frontend/src/components/Header.tsx
+++ emissions-app/frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
     <header className="bg-gray-800 text-white p-4">
-      <h1>Marino Maths Week</h1>
+      <h1><a href="https://marinomaths.com">Marino Maths Week</a></h1>
     </header>
 )
 
