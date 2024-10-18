cbdd9894b3f8a4918ae922bab4def57cc06f28b2
Jared Porcenaluk <jporcenaluk@gmail.com>
2024-10-18 09:02:45 +0100

    added the fix for tailwind@

--- emissions-app/frontend/tailwind.config.js
+++ emissions-app/frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
@@ -24,6 +25,6 @@ export default {
       }
     },
   },
-  plugins: [ require('daisyui') ],
+  plugins: [ daisyui ],
 }
 
